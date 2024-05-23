ff3f8639f9090a23309387a194930faada824f81
Costel <costelmartinescu2000@gmail.com>
2024-05-23 14:22:59 +0300

    multerConfig

--- pachete/multerConfig.js
+++ pachete/multerConfig.js
@@ -12,6 +12,7 @@ const storage = multer.diskStorage({
         cb(null, "client/public/uploads");
     }
   },
+  
   filename: function (req, file, cb) {
     cb(null, file.fieldname + "-" + file.originalname + "-" + Date.now() + ".jpg");
   },
